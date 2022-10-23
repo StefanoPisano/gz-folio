f05c80dbee8ed11ab1dd44bb0fbc1cb79459614b
Stefano Pisano <s.pisano@techgap.it>
2022-10-23 15:43:30 +0200

    fix

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,5 @@ module.exports = defineConfig({
 })
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/hello-world/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/stefanopisano-me/' : '/'
 }
