d9274ca6d82845848ceb51b25ec6fdb7b0f4fd33
Stefano Pisano <s.pisano@techgap.it>
2023-02-06 14:35:58 +0100

    fix(UI): portfolio url

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,5 @@ module.exports = defineConfig({
 })
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/stefanopisano-me/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/'
 }
