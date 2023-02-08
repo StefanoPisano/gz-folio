aaf706d740c6e9e36ec7ddccfa2c3059212a9e85
Stefano Pisano <s.pisano@techgap.it>
2023-02-08 19:00:19 +0100

    revert(config): public path for local env

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,5 @@ module.exports = defineConfig({
 })
 
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/portfolio/'
+	publicPath: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/'
 }
