d927e3dcb7da6763fd135303db473b3ffb96af19
Stefano Pisano <s.pisano@techgap.it>
2023-02-13 11:52:19 +0100

    build(deploy): fixed git pointing

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:StefanoPisano/stefanopisano-me.git master:gh-pages
+git push -f git@github.com:StefanoPisano/portfolio.git master:gh-pages
 cd -
