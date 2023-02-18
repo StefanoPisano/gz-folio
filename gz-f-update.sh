3134de10fd5140e33b5fc20a5ee9154e7cd081b6
Stefano Pisano <s.pisano@techgap.it>
2023-02-18 10:37:58 +0100

    build(update): add commit of sh update file

--- gz-f-update.sh
+++ gz-f-update.sh
@@ -8,4 +8,4 @@ git checkout main-repo/main -- babel.config.js
 git checkout main-repo/main -- CHANGELOG.md
 
 
-git commit package.json ./*.vue jsconfig.json src/router/routes.js public/index.html babel.config.js CHANGELOG.md -m "build(gz-folio): upgrade of gz-folio framework"
+git commit gz-f-update.sh package.json ./*.vue jsconfig.json src/router/routes.js public/index.html babel.config.js CHANGELOG.md -m "build(gz-folio): upgrade of gz-folio framework"
