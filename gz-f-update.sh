ddc3117edcf783ecc75dca34bac3b37f8ae55bec
Stefano Pisano <s.pisano@techgap.it>
2023-02-18 10:31:44 +0100

    build(update): improve update sh script

--- gz-f-update.sh
+++ gz-f-update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-git checkout main-repo/main -- public/index.html
+git checkout main-repo/main -- package.json
 git checkout main-repo/main -- *.vue
 git checkout main-repo/main -- src/router/routes.js
 git checkout main-repo/main -- jsconfig.json
@@ -8,4 +8,4 @@ git checkout main-repo/main -- babel.config.js
 git checkout main-repo/main -- CHANGELOG.md
 
 
-git commit ./*.vue jsconfig.json src/router/routes.js public/index.html babel.config.js CHANGELOG.md -m "build(gz-folio): upgrade of gz-folio framework"
+git commit package.json ./*.vue jsconfig.json src/router/routes.js public/index.html babel.config.js CHANGELOG.md -m "build(gz-folio): upgrade of gz-folio framework"
