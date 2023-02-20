b17d2a703c374dff977d7043ec14ba7bc20c15d4
Stefano Pisano <s.pisano@techgap.it>
2023-02-20 16:15:56 +0100

    build(version): added npm install after upgrade

--- gz-f-update.sh
+++ gz-f-update.sh
@@ -11,4 +11,4 @@ git checkout main-repo/main -- CHANGELOG.md
 git commit gz-f-update.sh package.json ./*.vue jsconfig.json src/router/routes.js public/index.html babel.config.js CHANGELOG.md -m "build(gz-folio): upgrade of gz-folio framework"
 
 
-npm run install
+npm install
