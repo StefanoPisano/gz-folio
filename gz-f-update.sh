5557a9338752181aaa209b36991b194268d33bc3
Stefano Pisano <s.pisano@techgap.it>
2023-03-04 09:43:57 +0100

    fix(gzfolio): fixed update scripts

--- gz-f-update.sh
+++ gz-f-update.sh
@@ -6,12 +6,12 @@ git checkout main-repo/main -- src/router/routes.js
 git checkout main-repo/main -- src/assets/img/gz-private/*
 git checkout main-repo/main -- jsconfig.json
 git checkout main-repo/main -- babel.config.js
-git checkout main-repo/main -- themes/default/theme.json
-git checkout main-repo/main -- themes/light/theme.json
+git checkout main-repo/main -- src/themes/default/theme.json
+git checkout main-repo/main -- src/themes/light/theme.json
 git checkout main-repo/main -- CHANGELOG.md
 
 
-git commit gz-f-update.sh package.json ./*.vue jsconfig.json src/router/routes.js src/assets/img/gz-private/* public/index.html babel.config.js themes/light/theme.json themes/default/theme.json CHANGELOG.md -m "build(gz-folio): upgrade of gz-folio framework"
+git commit gz-f-update.sh package.json ./*.vue jsconfig.json src/router/routes.js src/assets/img/gz-private/* public/index.html babel.config.js src/themes/light/theme.json src/themes/default/theme.json CHANGELOG.md -m "build(gz-folio): upgrade of gz-folio framework"
 
 
 npm install
